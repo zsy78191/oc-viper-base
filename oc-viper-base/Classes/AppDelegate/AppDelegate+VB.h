b7eb65287ad09d3935c1848c1c4f2e9f276eecf6
zsy78191 <zcleeco@qq.com>
2019-09-16 16:48:58 +0800

    0.1.1

---
+++ oc-viper-base/Classes/AppDelegate/AppDelegate+VB.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate+VB.h
+//  viper-base
+//
+//  Created by 张超 on 2019/8/26.
+//  Copyright © 2019 orzer. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AppDelegate (VB) {
+    
+}
+
+- (void)registDefault;
+- (void)registRouter;
+- (void)useCoreData;
+- (void)classy;
+ 
+
+@end
+
+NS_ASSUME_NONNULL_END
